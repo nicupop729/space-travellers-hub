4079d1bb9180af206da7a74ddd75a08ca0401688
Nicolae Pop <nicupop729@gmail.com>
2021-12-13 16:27:27 -0300

    feat: build app repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Rockets />} />
+      <Route exact path="/missions" element={<Missions />} />
+      <Route exact path="/profile" element={<Profile />} />
+    </Routes>
+  </div>
+);
 
 export default App;
