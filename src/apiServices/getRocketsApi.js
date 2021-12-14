2d919913cae0901ea8b28723e45588aad9c0262c
Nicolae Pop <nicupop729@gmail.com>
2021-12-14 17:45:21 -0300

    fix: update the Api function

--- src/apiServices/getRocketsApi.js
+++ src/apiServices/getRocketsApi.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-
 const GET_ROCKETS_API = 'https://api.spacexdata.com/v3/rockets';
 
-const fetchRockets = () => axios({
-  method: 'get',
-  url: GET_ROCKETS_API,
-});
+const fetchRockets = async () => {
+  const data = await fetch(GET_ROCKETS_API);
+  const dataResult = await data.json();
+  return dataResult;
+};
 
 export default fetchRockets;
