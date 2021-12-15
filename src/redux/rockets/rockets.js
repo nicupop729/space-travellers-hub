9591ca4190911cf7261b8aaee945fe1e54c57cb8
Nicolae Pop <nicupop729@gmail.com>
2021-12-15 16:36:36 -0300

    feat: build func and display rockets' reservation

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,15 +1,28 @@
 import fetchRockets from '../../apiServices/getRocketsApi';
 
 const ROCKET = 'redux/actions/get_rockets';
+const RESERVE_ROCKET = 'redux/actions/reserve_rockets';
 
-const rockets = (rockets) => ({
+const getRocketsAction = (rockets) => ({
   type: ROCKET,
   payload: rockets,
 });
 
+export const reserveRocketAction = (rocketID) => ({
+  type: RESERVE_ROCKET,
+  payload: rocketID,
+});
+
 export const getRockets = () => (dispach) => {
-  fetchRockets().then((rocket) => {
-    dispach(rockets(rocket));
+  fetchRockets().then((data) => {
+    const rockets = data.map((rocket) => ({
+      id: rocket.id,
+      name: rocket.rocket_name,
+      description: rocket.description,
+      image: rocket.flickr_images[0],
+      reserved: false,
+    }));
+    dispach(getRocketsAction(rockets));
   });
 };
 
@@ -17,6 +30,9 @@ const rocketsReducer = (state = [], action) => {
   switch (action.type) {
     case ROCKET:
       return action.payload;
+    case RESERVE_ROCKET:
+      return state.map((rocket) => (rocket.id === action.payload
+        ? { ...rocket, reserved: !rocket.reserved } : { ...rocket }));
     default:
       return state;
   }
