850906b56da0682edc5a0880d0213e770b1c83e8
Chike1990 <chibuikeuzoechina@gmail.com>
2021-12-16 03:16:09 +0100

    Implements join and leave action

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,14 +1,19 @@
 /* eslint-disable camelcase */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMissions } from '../../redux/missions/missions';
+import { getMissions, joinMissionAction } from '../../redux/missions/missions';
 import './missions.css';
 
-const Rockets = () => {
+let loadOance = false;
+
+const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missionsReducer);
   useEffect(() => {
-    dispatch(getMissions());
+    if (!loadOance) {
+      dispatch(getMissions());
+      loadOance = true;
+    }
   }, [dispatch]);
 
   return (
@@ -16,27 +21,61 @@ const Rockets = () => {
       <table className="Missions__table">
         <thead>
           <tr>
-            <th>Mission</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th>Join</th>
+            <th>
+              <h1>Mission</h1>
+            </th>
+            <th>
+              <h1>Description</h1>
+            </th>
+            <th>
+              <h1>Status</h1>
+            </th>
+            <th>
+              <h1>Join</h1>
+            </th>
           </tr>
         </thead>
-        {
-          missions.map(({
-            mission_id, mission_name, description, reserved = false,
-          }) => (
-            <tr key={mission_id}>
-              <td className="Missions__name"><h1>{ mission_name }</h1></td>
-              <td className="Missions__description"><p>{ description }</p></td>
-              <td>{ reserved ? (<span className="badge badge--primary">ACTIVE MEMBER</span>) : (<span className="badge badge--secondary">NOT A MEMBER</span>) }</td>
-              <td>{ reserved ? (<button type="button" className="app-btn-danger">Leave Mission</button>) : (<button type="button" className="app-btn-ghost">Join Mission</button>) }</td>
-            </tr>
-          ))
-        }
+        {missions.map(({
+          id, name, description, reserved,
+        }) => (
+          <tr key={id}>
+            <td className="Missions__name">
+              <h1>{name}</h1>
+            </td>
+            <td className="Missions__description">
+              <p>{description}</p>
+            </td>
+            <td>
+              {reserved ? (
+                <span className="badge badge--primary">ACTIVE MEMBER</span>
+              ) : (
+                <span className="badge badge--secondary">NOT A MEMBER</span>
+              )}
+            </td>
+            <td>
+              {reserved ? (
+                <button
+                  type="button"
+                  onClick={() => dispatch(joinMissionAction(id))}
+                  className="app-btn-danger"
+                >
+                  Leave Mission
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() => dispatch(joinMissionAction(id))}
+                  className="app-btn-ghost"
+                >
+                  Join Mission
+                </button>
+              )}
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );
 };
 
-export default Rockets;
+export default Missions;
